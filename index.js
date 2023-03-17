ce9815a4cd54aceaff696e072b82fe96c9b3015c
Vivek-Malla <thisisvivekmalla@gmail.com>
2023-03-17 19:14:57 +0530

    Only with javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+let num;
+let main;
+function display(num){
+    head.value += num;
+}
+function solve(main){
+    console.log(main);
+    head.value = eval(main);
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+let num;
+let main;
+function display(num: any) :number{
+ return num;
+}
+function solve(main: any){
+    console.log(main);
+}
